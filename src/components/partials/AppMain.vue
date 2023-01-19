934808d1c3225c160de6bce8fa970a89289aeadf
Mariosam2 <marco.mariosaa@gmail.com>
2023-01-19 18:31:36 +0100

    adding pagination feature

--- src/components/partials/AppMain.vue
+++ src/components/partials/AppMain.vue
@@ -9,24 +9,37 @@ export default {
     data() {
         return {
             baseURL: 'http://127.0.0.1:8000/api',
-            projects: []
+            projects: null,
+            currentPage: 1,
+            prevPageUrl: null,
+            nextPageUrl: null
         }
     },
     methods: {
-        getProjects() {
-            axios.get(this.baseURL + '/projects')
+        getProjects(url) {
+            axios.get(url)
                 .then(resp => {
                     //console.log(resp.data.result);
-                    this.projects = resp.data.result;
+                    this.projects = resp.data.result.data;
+                    this.currentPage = resp.data.result.current_page;
+                    this.prevPageUrl = resp.data.result.prev_page_url;
+                    this.nextPageUrl = resp.data.result.next_page_url;
+
                 })
-                .catch(function (err) {
+                .catch(err => {
                     console.log(err);
                 })
+        },
+        prevPage(url) {
+            this.getProjects(url);
+        },
+        nextPage(url) {
+            this.getProjects(url);
         }
 
     },
     mounted() {
-        this.getProjects()
+        this.getProjects(this.baseURL + '/projects')
         //console.log(this.projects)
 
     }
@@ -34,15 +47,30 @@ export default {
 </script>
 <template>
     <main id="site_main">
-        <div class="container p-5">
+        <div class="container pt-5 min-vh-100">
             <div class="row row-cols-1 row-cols-md-3 g-4">
-                <!-- TODO: make get request with axios in ProjectCard Component + iteraion to show all projects-->
                 <ProjectCard v-for="project in projects" :project="project"></ProjectCard>
             </div>
+            <div class="d-flex justify-content-center mt-5">
+                <div class="prev" v-if="prevPageUrl != null" @click="prevPage(prevPageUrl)">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                        class="bi bi-arrow-bar-left ms_icon " viewBox="0 0 16 16">
+                        <path fill-rule="evenodd"
+                            d="M12.5 15a.5.5 0 0 1-.5-.5v-13a.5.5 0 0 1 1 0v13a.5.5 0 0 1-.5.5ZM10 8a.5.5 0 0 1-.5.5H3.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L3.707 7.5H9.5a.5.5 0 0 1 .5.5Z" />
+                    </svg>
+                </div>
+                <span class="fw-bold fs-5 mx-2">{{ currentPage }}</span>
+                <div class="next" v-if="nextPageUrl != null" @click="nextPage(nextPageUrl)">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                        class="bi bi-arrow-bar-right ms_icon" viewBox="0 0 16 16">
+                        <path fill-rule="evenodd"
+                            d="M6 8a.5.5 0 0 0 .5.5h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L12.293 7.5H6.5A.5.5 0 0 0 6 8Zm-2.5 7a.5.5 0 0 1-.5-.5v-13a.5.5 0 0 1 1 0v13a.5.5 0 0 1-.5.5Z" />
+                    </svg>
+                </div>
+            </div>
         </div>
-
     </main>
 </template>
-<style>
+<style lang="scss" scoped>
 
 </style>
