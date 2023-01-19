759caed4af14361f227afe6762fb58abf01589b6
Mariosam2 <marco.mariosaa@gmail.com>
2023-01-19 14:02:49 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+  data() {
+    return {
+      message: 'Hello Vite + Vue',
+    }
+  },
+  methods: {
+
+  },
+  mounted() {
+
+  }
+}
+</script>
+
+<template>
+  <div>{{ message }}</div>
+</template>
+
+<style lang="scss">
+
+</style>
+
---
+++ src/main.js
@@ -0,0 +1,7 @@
+import { createApp } from 'vue'
+import './assets/scss/app.scss'
+import "bootstrap/scss/bootstrap.scss";
+import "bootstrap/dist/js/bootstrap.bundle.js";
+import App from './App.vue'
+
+createApp(App).mount('#app')
