4c2bed1859e3c70307ea94fc7fc72a014405a925
Mariosam2 <marco.mariosaa@gmail.com>
2023-01-30 04:54:13 +0100

    edited projects layout and style

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -25,34 +25,92 @@ export default {
 </script>
 <template>
     <div class="col">
-        <div class="card ms_card text_primary bg_primary">
-            <img class="card-img-top" :src="store.getImage(project.img)" :alt="project.slug">
-            <div class="card-body">
-                <h4 class="card-title">{{ project.title }}</h4>
-                <p class="card-text">{{ trimDescription + '...'}}
-                </p>
-                <p class="card-text m-0 py-2" v-if="project.type"><strong>Type:</strong> {{ project.type.name }}</p>
-                <p class="card-text m-0 py-2" v-else><strong>Type:</strong> <span>None</span> </p>
-                <p class="card-text m-0 py-2" v-if="project.technologies.length > 0">
-                    <strong>Technologies: </strong>
-                    <span v-for="(technology, i) in project.technologies">
-                        {{ !(i === project.technologies.length - 1) ? technology.name + ', ' : technology.name}}
-                    </span>
-                </p>
-                <p class="card-text m-0 py-2" v-else><strong>Technologies:</strong> <span>None</span> </p>
-            </div>
-            <div class="card-footer bg_secondary d-flex justify-content-between">
-                <router-link class="btn bg_primary d-block ms-auto"
-                    :to="{ name: 'project', params: { slug: project.slug } }">More
-                    details</router-link>
-
-            </div>
+        <div class="project rounded-2">
+            <div class="project-layover rounded-2"></div>
+            <img class="project-img rounded-2" :src="store.getImage(project.img)" :alt="project.name">
+            <router-link class="project-link" :to="{ name: 'project', params: { slug: project.slug } }">
+                More details...
+            </router-link>
+            <router-link class="btn bg_primary d-none ms-auto"
+                :to="{ name: 'project', params: { slug: project.slug } }">More
+                details
+            </router-link>
         </div>
+
+
     </div>
 </template>
 <style lang="scss" scoped>
-.ms_card {
-    height: 100%;
-    border: none;
+.project {
+    width: 100%;
+    height: 320px;
+    position: relative;
+    overflow: hidden;
+
+    &:hover {
+        cursor: pointer;
+    }
+
+    .project-layover {
+        position: absolute;
+        width: 100%;
+        height: 100%;
+        background-color: rgba(black, 0.5);
+        z-index: 999;
+
+    }
+
+    .project-img {
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+        transition: all 0.25s ease-out;
+
+        &:before {
+            content: "";
+            display: none;
+            height: 100%;
+            width: 100%;
+            position: absolute;
+            top: 0;
+            left: 0;
+
+
+        }
+
+    }
+
+    &:hover .project-img {
+        scale: 1.1;
+
+    }
+
+    &:hover .project-img:before {
+        display: block;
+    }
+
+
+
+
+    .project-link {
+        opacity: 0;
+        position: absolute;
+        left: 50%;
+        top: 80%;
+        translate: -50% -50%;
+        z-index: 99999;
+        color: var(--text-almost-white);
+        text-decoration: none;
+        transition: all 0.25s ease-in-out;
+
+        &:hover {
+            text-decoration: underline;
+
+        }
+    }
+
+    &:hover .project-link {
+        opacity: 1;
+    }
 }
 </style>
